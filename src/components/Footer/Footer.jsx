77910cac38da1a9ee019e46b9a5cb93605d76a4e
thomcodev <thomspachecop@gmail.com>
2024-07-15 21:59:06 -0500

    nuevo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,23 +6,23 @@ const Footer = () => {
             <div className='flex flex-wrap justify-between opacity-80'>
                 <ul className='flex gap-6'>
                     <li>
-                        <a href="https://www.linkedin.com/in/erick-josep-salazar-8b8b6024b/" target="_blank" >
+                        <a href="" target="_blank" >
                             <BsLinkedin size={16} />
                         </a>
                     </li>
                     <li>
-                        <a href="https://github.com/ErickSalazar44/aluraFlix" target="_blank">
+                        <a href="" target="_blank">
                             <BsGithub size={16} />
                         </a>
                     </li>
                     <li>
-                        <a href="https://portafolio-ericksalazar.netlify.app/" target="_blank">
+                        <a href="" target="_blank">
                             <BsFillBriefcaseFill size={16}/>
                         </a>
                     </li>
                 </ul>
                 <div>
-                    <p className='text-xs font-medium'>Made with 💙 by Erick</p>
+                    <p className='text-xs font-medium'>DEV PEDRO </p>
                 </div>
             </div>
         </footer>
